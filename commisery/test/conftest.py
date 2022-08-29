32dfa61acd3f8f80f398f132446cfbda56801970
Joost Muller <git@mllr.dev>
2022-08-29 14:59:53 +0200

    test: extend `commisery_cli` with tag and custom command support
    Also, the `rev_range` parameter may be an empty string to not pass a
    target to the CLI.

--- commisery/test/conftest.py
+++ commisery/test/conftest.py
@@ -19,6 +19,7 @@ import types
 import git
 import pytest
 from click.testing import CliRunner
+from .util import CommitAndTag
 
 try:
     # Python >= 3.8
@@ -36,37 +37,57 @@ commisery_range_cli = [ep for ep in _eps if ep.name == "cm"][0].load()
 
 @pytest.fixture()
 def commisery_cli():
-    def _cli(commit_messages, rev_range=None, with_tags=(), verbose=True):
+    def _cli(
+        commit_messages,
+        rev_range=None,
+        with_tags=(),
+        verbose=True,
+        command="check",
+    ):
         runner = CliRunner(mix_stderr=False)
         with runner.isolated_filesystem():
             with git.Repo.init() as repo:
                 if isinstance(commit_messages, types.GeneratorType):
                     commit_messages = list(commit_messages)
-                for msg in commit_messages:
-                    repo.index.commit(msg, author=git.Actor("Bob", "bob@tester.org>"))
+                for commit in commit_messages:
+                    if isinstance(commit, CommitAndTag):
+                        message = commit.message
+                        tag = commit.tag
+                    else:
+                        message = commit
+                        tag = None
+                    with open("test_file", "a") as f:
+                        f.write(".")
+                    repo.index.add(("test_file",))
 
-                if rev_range is None:
-                    # We'll check all messages if revision range is not provided
-                    rev_range = ("HEAD",)
+                    assert isinstance(message, str)
+                    repo.index.commit(message, author=git.Actor("Bob", "bob@tester.org>"))
 
-                assert isinstance(rev_range, collections.abc.Sequence)
+                    if tag is not None:
+                        assert isinstance(tag, str)
+                        repo.create_tag(path=tag, ref="HEAD")
 
                 args = ["-v", "DEBUG"] if verbose else []
                 if with_tags:
                     args.extend(("-t", ",".join(with_tags)))
-                args.append("check")
-                args.extend(rev_range)
+                args.append(command)
+
+                if rev_range is None:
+                    # We'll check all messages if revision range is not provided by the test
+                    rev_range = ("HEAD",)
+                assert isinstance(rev_range, collections.abc.Sequence)
+
+                # Empty string means default/not-defined
+                if rev_range != "":
+                    args.extend(rev_range)
 
                 result = runner.invoke(commisery_range_cli, args=args, color=True)
-
                 if result.stdout_bytes:
                     print(result.stdout, end="")
                 if result.stderr_bytes:
                     print(result.stderr, end="", file=sys.stderr)
 
-                if result.exception is not None and not isinstance(
-                    result.exception, SystemExit
-                ):
+                if result.exception is not None and not isinstance(result.exception, SystemExit):
                     raise result.exception
 
                 return result
